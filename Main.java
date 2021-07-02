551d550ff6e751d13e1e7fda6349a59a9bd60bfd
hashar7 <86832867+hashar7@users.noreply.github.com>
2021-07-02 16:11:06 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+    static public void main(String[] args) {
+        ControlFlowStatements1Impl test = new ControlFlowStatements1Impl();
+	    System.out.println("Hello, World!");
+	    System.out.println("Function at x = PI: " + test.getFunctionValue((float) Math.PI));
+        System.out.println("Function at x = -PI: " + test.getFunctionValue((float) -Math.PI));
+        System.out.println("Decoding weekday number 4: " + test.decodeWeekday(4));
+        System.out.println("Minimal element of array: " + test.getMinValue(test.initArray()));
+        System.out.println("Years when 100%: " + test.calculateBankDeposit(100.0).years);
+    }
+}
