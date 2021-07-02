4567204976b6f8492c9fc77cd2a859b788bba820
hashar7 <86832867+hashar7@users.noreply.github.com>
2021-07-02 16:11:50 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,8 +3,8 @@ package com.company;
 public class Main {
     static public void main(String[] args) {
         ControlFlowStatements1Impl test = new ControlFlowStatements1Impl();
-	    System.out.println("Hello, World!");
-	    System.out.println("Function at x = PI: " + test.getFunctionValue((float) Math.PI));
+	System.out.println("Hello, World!");
+	System.out.println("Function at x = PI: " + test.getFunctionValue((float) Math.PI));
         System.out.println("Function at x = -PI: " + test.getFunctionValue((float) -Math.PI));
         System.out.println("Decoding weekday number 4: " + test.decodeWeekday(4));
         System.out.println("Minimal element of array: " + test.getMinValue(test.initArray()));
