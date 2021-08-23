edb5fa91b98d1bff1326cfc7c36f129144f70ade
hashar7 <86832867+hashar7@users.noreply.github.com>
2021-08-23 18:54:14 +0300

    Create NW-7.8.sql

---
+++ SQL/Analytic functions/NW-7.8.sql
@@ -0,0 +1,17 @@
+/*
+Для каждой делавшей заказы компании из таблицы CUSTOMERS вывести имя заказчика (companyname), а тремя следующими столбцами - минимальную, максимальную и среднюю
+стоимость заказов этого заказчика с учетом скидки (данные из таблиц ORDERS и ORDERDETAILS).
+Примечание: предполагается, что запрос будет содержать только одно слово SELECT
+*/
+
+select distinct c.CompanyName,
+
+MIN(UnitPrice * Quantity * (1 - Discount)) 
+OVER (PARTITION BY o.customerid) AS "MIN",
+MAX(UnitPrice * Quantity * (1 - Discount)) 
+OVER (PARTITION BY o.customerid) AS "MAX",
+AVG(UnitPrice * Quantity * (1 - Discount)) 
+OVER (PARTITION BY o.customerid) AS "AVG"
+
+FROM Customers c, Orders o, OrderDetails od
+WHERE c.CustomerId = o.CustomerID AND o.OrderID = od.OrderID
