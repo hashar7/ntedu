7068e4d82aeac30c439d3ce436ddfa923f32121e
hashar7 <86832867+hashar7@users.noreply.github.com>
2021-08-23 14:36:31 +0300

    Create 6-1.sql

---
+++ SQL/SQL-part-2-TEST-v1/6-1.sql
@@ -0,0 +1,13 @@
+/*
+Для сотрудника с employee_id = 110 выведите полную строку его подчинения, где менеджеры разделены символом #.
+Имя сотрудника следует выводить в формате first_name last_name.
+Пример строки подчинения: #employee#manager#manager_of_manager
+(первым должен идти сам сотрудник, затем - его непосредственный менеджер, ..., последним - топ-менеджер).
+Указание: использование SYS_CONNECT_BY_PATH.
+*/
+
+select SYS_CONNECT_BY_PATH(first_name || ' ' || last_name, '#')
+from employees
+where manager_id is null
+start with employee_id = 110
+connect by prior manager_id = employee_id
