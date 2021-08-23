694d7f950853f52aef1777ee84875931741992ac
hashar7 <86832867+hashar7@users.noreply.github.com>
2021-08-23 14:40:31 +0300

    Create 6-5.sql

---
+++ SQL/SQL-part-2-TEST-v1/6-5.sql
@@ -0,0 +1,10 @@
+/*
+Вывести сотрудников, находящихся в транзитивном (не прямом) подчинении у сотрудника с id = 102
+О каждом сотруднике вывести employee_id, first_name, last_name.
+*/
+
+select employee_id, first_name, last_name
+from employees
+where level > 2
+start with employee_id = 102
+connect by manager_id = prior employee_id
