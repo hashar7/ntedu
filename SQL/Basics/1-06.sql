f823678f4083ff88e031db291642e4c8e3d19538
hashar7 <86832867+hashar7@users.noreply.github.com>
2021-08-23 17:52:28 +0300

    Create 1-06.sql

---
+++ SQL/Basics/1-06.sql
@@ -0,0 +1,7 @@
+/*
+Выведите уникальный набор из id должностей сотрудников.
+Указание: Использовать только таблицу employees, а также ключевое слово distinct или unique
+*/
+
+select distinct job_id
+from employees
