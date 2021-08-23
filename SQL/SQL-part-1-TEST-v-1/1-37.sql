ffa6162962dfc94cdda77434aa2f6349d3842b55
hashar7 <86832867+hashar7@users.noreply.github.com>
2021-08-23 14:28:08 +0300

    Create 1-37.sql

---
+++ SQL/SQL-part-1-TEST-v-1/1-37.sql
@@ -0,0 +1,9 @@
+/*
+Для сотрудника по фамилии 'Vargas' вывести сумму денег, на которую его зарплата
+меньше зарплаты менеджера отдела, в котором работает этот сотрудник.
+Примечание. Комиссионные к задаче отношения не имеют.
+/*
+
+select m.salary - e.salary
+from employees e, employees m, departments d
+where e.last_name = 'Vargas' and e.department_id = d.department_id and m.employee_id = d.manager_id
